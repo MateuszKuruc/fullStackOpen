1752f6b3f857f122ecec4c68cbb4e8c7211860a5
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-08 17:39:54 +0200

    ex.1.6 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const title = "give feedback";
+  const statistics = "statistics";
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodFeedback = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralFeedback = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadFeedback = () => {
+    setBad(bad + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title title={title} />
+      <Button handleClick={handleGoodFeedback} text="good" />
+      <Button handleClick={handleNeutralFeedback} text="neutral" />
+      <Button handleClick={handleBadFeedback} text="bad" />
+      <Statistics stats={statistics} />
+      <Total value={good} text='good' />
+      <Total value={neutral} text='neutral' />
+      <Total value={bad} text='bad' />
     </div>
   );
+};
+
+const Title = (props) => (
+  <div>
+    <h1>{props.title}</h1>
+  </div>
+);
+const Statistics = (props) => (
+  <div>
+    <h1>{props.stats}</h1>
+  </div>
+);
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const Total = ({ value, text }) => {
+  return (
+    <p>{text} {value}</p>
+  )
 }
 
 export default App;
