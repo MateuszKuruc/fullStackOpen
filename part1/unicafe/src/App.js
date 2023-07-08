81edf668ead6bb79154844ac534569cc6de8c0a9
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-08 18:32:31 +0200

    ex 1.7 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,17 +7,31 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const calcAverage = () => {
+    if (total === 0) return
+    return Number((good * 1 + neutral * 0 + bad * -1) / total);
+  };
+
+  const calcPositive = () => {
+    if (total === 0) return
+    return (Number((good / total) * 100));
+  }
 
   const handleGoodFeedback = () => {
     setGood(good + 1);
+    setTotal(total + 1);
   };
 
   const handleNeutralFeedback = () => {
     setNeutral(neutral + 1);
+    setTotal(total + 1);
   };
 
   const handleBadFeedback = () => {
     setBad(bad + 1);
+    setTotal(total + 1);
   };
 
   return (
@@ -27,9 +41,12 @@ const App = () => {
       <Button handleClick={handleNeutralFeedback} text="neutral" />
       <Button handleClick={handleBadFeedback} text="bad" />
       <Statistics stats={statistics} />
-      <Total value={good} text='good' />
-      <Total value={neutral} text='neutral' />
-      <Total value={bad} text='bad' />
+      <Stats value={good} text="good" />
+      <Stats value={neutral} text="neutral" />
+      <Stats value={bad} text="bad" />
+      <Stats value={total} text="total" />
+      <Stats value={calcAverage()} text="average" />
+      <Stats value={calcPositive()} text='positive' percent='%' />
     </div>
   );
 };
@@ -48,10 +65,12 @@ const Button = (props) => {
   return <button onClick={props.handleClick}>{props.text}</button>;
 };
 
-const Total = ({ value, text }) => {
+const Stats = ({ value, text, percent }) => {
   return (
-    <p>{text} {value}</p>
-  )
-}
+    <p>
+      {text} {value} {percent}
+    </p>
+  );
+};
 
 export default App;
