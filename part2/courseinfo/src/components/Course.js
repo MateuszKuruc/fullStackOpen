7653f7e6978fe2f4729c175b16ae4ad69bd2be83
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-10 01:08:57 +0200

    ex 2.2 done

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,10 +1,16 @@
 const Course = ({ course }) => {
+    const exercises = course.parts.map(part => part.exercises);
+    console.log(exercises);
+    const total = exercises.reduce((total, exercises) => total + exercises,0);
+    console.log(total);
+  
   return (
     <div>
       <h1>{course.name}</h1>
       <ul>
         {course.parts.map(part => <li key={part.id}>{part.name} {part.exercises}</li>)}
       </ul>
+      <h2>total of {total}</h2>
     </div>
   );
 };
