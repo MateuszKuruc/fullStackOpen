64f5e7257a36ba6f3507a6f230c49e6b2781615a
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-10 00:48:01 +0200

    ex 2.1 done

---
+++ part2/learning/src/App.js
@@ -0,0 +1,22 @@
+import Note from "./components/Note";
+
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {/* <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li> */}
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+          // <li key={note.id}>
+          //   {note.content}
+          // </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
