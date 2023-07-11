2954ed620b6a905797f6eb764d219c2d77207b2e
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-11 16:23:03 +0200

    phonebook ex 2.6 done

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const createNewName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addNewName = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={createNewName} />
+        </div>
+        <div>
+          <button type="submit" onClick={addNewName}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>
+          <p>{person.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
