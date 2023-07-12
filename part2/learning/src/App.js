6bd90d79516dc90be0d68b0bd970d1c445557f2e
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-12 13:06:36 +0200

    data fetching exercises

--- part2/learning/src/App.js
+++ part2/learning/src/App.js
@@ -1,34 +1,24 @@
 import Note from "./components/Note";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
-    console.log('effect');
-    axios
-    .get('http://localhost:3001/notes')
-    .then(response => {
-      console.log('promise fulfilled');
-      setNotes(response.data)
-    })
-  }
-
-  useEffect(hook, []);
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
 
   // useEffect(() => {
-  //   console.log('effect');
-  //   axios
-  //   .get('http://localhost:3001/notes')
-  //   .then(response => {
-  //     console.log('promise fulfilled');
+  //   axios.get("http://localhost:3001/notes").then((response) => {
   //     setNotes(response.data);
-  //   })
-  // }, [])
-  console.log('render', notes.length, 'notes');
+  //   });
+  // }, []);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -36,11 +26,21 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1,
+      // id: notes.length + 1,
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNotes("");
+    });
+
+    // axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+    //   setNotes(notes.concat(response.data));
+    //   setNewNote("");
+    // });
+
+    // setNotes(notes.concat(noteObject));
+    // setNewNote("");
   };
 
   const handleNoteChange = (event) => {
@@ -48,21 +48,38 @@ const App = () => {
     setNewNote(event.target.value);
   };
 
-  const notesToShow = showAll 
+  const notesToShow = showAll
     ? notes
-    : notes.filter(note => note.important === true)
+    : notes.filter((note) => note.important === true);
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService.update(id, changedNote).then((returnedNote) => {
+      setNotes(notes.map((n) => (n.id !== n ? n : returnedNote)));
+    });
+
+    // axios.put(url, changedNote).then(response => {
+    //   setNotes(notes.map(n => n.id !== id ? n : response.data))
+    // })
+  };
 
   return (
     <div>
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? "important" : "all"}
         </button>
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
