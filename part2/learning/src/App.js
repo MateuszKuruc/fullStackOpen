c3e7e4cbc19b95989a9b7a340e7dd09034e9d4da
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-25 13:53:43 +0200

    add state control for user infi, username, password. Refactor code

--- part2/learning/src/App.js
+++ part2/learning/src/App.js
@@ -4,22 +4,20 @@ import Footer from "./components/Footer";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import noteService from "./services/notes";
+import loginService from "./services/login";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
-  const [errorMessage, setErrorMessage] = useState('some error happened')
+  const [errorMessage, setErrorMessage] = useState("some error happened");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
       setNotes(initialNotes);
-    });
-  });
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/notes").then((response) => {
-      setNotes(response.data);
     });
   }, []);
 
@@ -49,24 +47,81 @@ const App = () => {
     const changedNote = { ...note, important: !note.important };
 
     noteService
-    .update(id, changedNote).then((returnedNote) => {
-      setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
-    })
-    .catch(error => {
-      setErrorMessage(
-        `Note '${note.content}' was already deleted from server`
-        )
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already deleted from server`
+        );
         setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-      setNotes(notes.filter(n => n.id !== id))
-    })
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
   };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange} />
+      <button type="submit">save</button>
+    </form>
+  );
 
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      {/* {user === null && loginForm()};{user !== null && noteForm()}; */}
+      {user === null ? loginForm() : noteForm()}
+      {user && <div>
+        <p>{user.name} logged in</p>
+        {noteForm}
+        </div>}
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -81,10 +136,10 @@ const App = () => {
           />
         ))}
       </ul>
-      <form onSubmit={addNote}>
+      {/* <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
-      </form>
+      </form> */}
       <Footer />
     </div>
   );
