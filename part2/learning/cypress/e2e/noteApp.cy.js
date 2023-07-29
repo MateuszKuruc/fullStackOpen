bafdf5ca9cf69e384299374c4d76a3a3e8320391
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-29 20:36:02 +0200

    refactor code, add chaining

--- part2/learning/cypress/e2e/noteApp.cy.js
+++ part2/learning/cypress/e2e/noteApp.cy.js
@@ -67,8 +67,9 @@ describe("Note app", function () {
     cy.get("#password").type("wrong");
     cy.get("#login-button").click();
 
-    cy.get(".error").should("contain", "Wrong credentials");
-    cy.get(".error").should("have.css", "color", "rgb(255, 0, 0)");
-    cy.get(".error").should("have.css", "border-style", "solid");
+    cy.get(".error")
+      .should("contain", "Wrong credentials")
+      .and("have.css", "color", "rgb(255, 0, 0)")
+      .and("have.css", "border-style", "solid");
   });
 });
