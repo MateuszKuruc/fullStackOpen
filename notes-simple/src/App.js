12afa3c919f8778efcc21997aceed4c210d40897
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-31 21:26:46 +0200

    edit reducer not to change state of the object

--- notes-simple/src/App.js
+++ notes-simple/src/App.js
@@ -1,9 +1,8 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
 const noteReducer = (state = [], action) => {
   if (action.type === "NEW_NOTE") {
-    state.push(action.payload);
-    return state;
+    return state.concat(action.payload);
   }
 
   return state;
@@ -12,34 +11,35 @@ const noteReducer = (state = [], action) => {
 const store = createStore(noteReducer);
 
 store.dispatch({
-  type: 'NEW_NOTE',
+  type: "NEW_NOTE",
   payload: {
-    content: 'the app state is in redux store',
+    content: "the app state is in redux store",
     important: true,
-    id: 1
-  }
-})
+    id: 1,
+  },
+});
 
 store.dispatch({
-  type: 'NEW_NOTE',
+  type: "NEW_NOTE",
   payload: {
-    content: 'state changes are made with actions',
+    content: "state changes are made with actions",
     important: false,
-    id: 2
-  }
-})
+    id: 2,
+  },
+});
 
 const App = () => {
   return (
     <div>
       <ul>
-        {store.getState().map(note => 
+        {store.getState().map((note) => (
           <li key={note.id}>
-            {note.content} <strong></strong>
-          </li>)}
+            {note.content} <strong>{note.important ? "important" : ""}</strong>
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default App;
