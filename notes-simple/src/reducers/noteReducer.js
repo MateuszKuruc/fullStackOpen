aa64947f9e56ee62253b65c0ea0486471d92ad22
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-31 22:22:59 +0200

    rewrite note reducer to handle both new note action and toggling importance

--- notes-simple/src/reducers/noteReducer.js
+++ notes-simple/src/reducers/noteReducer.js
@@ -1,9 +1,19 @@
 const noteReducer = (state = [], action) => {
-    if (action.type === "NEW_NOTE") {
+  switch (action.type) {
+    case "NEW_NOTE":
       return state.concat(action.payload);
+    case "TOGGLE_IMPORTANCE": {
+      const id = action.payload.id;
+      const noteToChange = state.find((n) => n.id === id);
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      };
+      return state.map((note) => (note.id !== id ? note : changedNote));
     }
-  
-    return state;
-  };
+    default:
+      return state;
+  }
+};
 
-  export default noteReducer
+export default noteReducer;
