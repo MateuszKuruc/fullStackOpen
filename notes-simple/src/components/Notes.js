b7b43226439f6b4165b109cd8363cbc329a9e91f
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-01 14:19:36 +0200

    Notes component updated

--- notes-simple/src/components/Notes.js
+++ notes-simple/src/components/Notes.js
@@ -1,11 +1,11 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleImportanceOf } from "../reducers/noteReducer";
 
 const Note = ({ note, handleClick }) => {
   return (
     <li onClick={handleClick}>
       {note.content}
-      <strong>{note.important ? "important" : ""}</strong>
+      <strong> {note.important ? "important" : ""}</strong>
     </li>
   );
 };
@@ -19,10 +19,12 @@ const Notes = () => {
       {notes.map((note) => (
         <Note
           key={note.id}
+          note={note}
           handleClick={() => dispatch(toggleImportanceOf(note.id))}
         />
       ))}
     </ul>
   );
 };
+
 export default Notes;
