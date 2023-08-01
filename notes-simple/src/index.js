aeb51f15923b8fc37955ca9d95fa9f262e0bd90d
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-01 13:32:19 +0200

    divide code between index, app and reducer files

--- notes-simple/src/index.js
+++ notes-simple/src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import noteReducer from "./reducers/noteReducer";
+
+const store = createStore(noteReducer);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
