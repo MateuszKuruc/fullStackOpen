5db04c394528da04b2b6b96ea79168d15b919263
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-02 11:38:49 +0200

    add combine reducers functions

--- notes-simple/src/reducers/noteReducer.js
+++ notes-simple/src/reducers/noteReducer.js
@@ -1,27 +1,15 @@
-// const initialState = [
-//   {
-//     content: "reducer defines how redux store works",
-//     important: true,
-//     id: 1,
-//   },
-//   {
-//     content: "state of store can contain any date",
-//     important: false,
-//     id: 2,
-//   },
-// ];
-
-const initialState = {
-  notes: [
-    {
-      content: "reducer defines how redux store works",
-      important: true,
-      id: 1,
-    },
-    { content: "state of store can contain any data", important: false, id: 2 },
-  ],
-  filter: "IMPORTANT",
-};
+const initialState = [
+  {
+    content: "reducer defines how redux store works",
+    important: true,
+    id: 1,
+  },
+  {
+    content: "state of store can contain any date",
+    important: false,
+    id: 2,
+  },
+];
 
 const noteReducer = (state = initialState, action) => {
   switch (action.type) {
