9dc8e53a68f59f0d9f0ad2dc23c51ea0d0cb56dd
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-03 11:47:42 +0200

    anecdote reducer file cleaned

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,44 +19,7 @@ const asObject = (anecdote) => {
   };
 };
 
-// export const getVote = (id) => {
-//   return {
-//     type: "VOTE",
-//     payload: { id },
-//   };
-// };
-
-// export const createAnecdote = (content) => {
-//   return {
-//     type: "NEW_ANECDOTE",
-//     payload: {
-//       content,
-//       id: getId(),
-//       votes: 0,
-//     },
-//   };
-// };
-
 const initialState = anecdotesAtStart.map(asObject);
-
-// const reducer = (state = initialState, action) => {
-//   // console.log("state now: ", state);
-//   // console.log("action", action);
-//   switch (action.type) {
-//     case "VOTE":
-//       const id = action.payload.id;
-//       const anecdoteToChange = state.find((a) => a.id === id);
-//       const changedAnecdote = {
-//         ...anecdoteToChange,
-//         votes: anecdoteToChange.votes + 1,
-//       };
-//       return state.map((a) => (a.id !== id ? a : changedAnecdote));
-//     case "NEW_ANECDOTE":
-//       return [...state, action.payload];
-//     default:
-//       return state;
-//   }
-// };
 
 const anecdoteSlice = createSlice({
   name: "anecdotes",
@@ -67,16 +30,9 @@ const anecdoteSlice = createSlice({
       const newAnecdote = {
         content,
         id: getId(),
-        votes: 0
-      }
-      return [...state, newAnecdote]
-
-      // state.push({
-      //   content,
-      //   id: getId(),
-      //   votes: 0,
-      // });
-    
+        votes: 0,
+      };
+      return [...state, newAnecdote];
     },
     getVote(state, action) {
       const id = action.payload;
