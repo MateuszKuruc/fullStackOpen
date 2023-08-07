91aa7d28ca2d28a222269253e89e8298ee0764e3
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-07 11:33:23 +0200

    add notes service to handle getting data from the backend

--- notes-simple/src/reducers/noteReducer.js
+++ notes-simple/src/reducers/noteReducer.js
@@ -1,23 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
-  {
-    content: "reducer defines how redux store works",
-    important: true,
-    id: 1,
-  },
-  {
-    content: "state of store can contain any date",
-    important: false,
-    id: 2,
-  },
-];
+// const initialState = [
+//   {
+//     content: "reducer defines how redux store works",
+//     important: true,
+//     id: 1,
+//   },
+//   {
+//     content: "state of store can contain any date",
+//     important: false,
+//     id: 2,
+//   },
+// ];
 
 const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 const noteSlice = createSlice({
   name: "notes",
-  initialState,
+  initialState: [],
   reducers: {
     createNote(state, action) {
       const content = action.payload;
@@ -34,6 +34,9 @@ const noteSlice = createSlice({
         ...noteToChange,
         important: !noteToChange.important,
       };
+
+      console.log(JSON.parse(JSON.stringify(state)));
+
       return state.map((note) => (note.id !== id ? note : changedNote));
     },
   },
