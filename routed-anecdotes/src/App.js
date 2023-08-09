421adbf1f2697458d0c0cbfbc4d84dea4c37c8d6
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-09 12:19:14 +0200

    clean code

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -7,19 +7,6 @@ import {
   useParams,
 } from "react-router-dom";
 
-// const Menu = () => {
-//   const padding = {
-//     paddingRight: 5,
-//   };
-//   // return (
-//   //   <div>
-//   //     <a href='#' style={padding}>anecdotes</a>
-//   //     <a href='#' style={padding}>create new</a>
-//   //     <a href='#' style={padding}>about</a>
-//   //   </div>
-//   // )
-// };
-
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
