fbbd41444f735a80ee2bf4ac5407fad3ed129aa4
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-23 17:35:28 +0200

    add state handling for editing author, add form for getting user input

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,12 +1,20 @@
 import { useMutation } from "@apollo/client";
 import { EDIT_AUTHOR } from "../queries";
+import { useState } from "react";
 
 const Authors = ({ show, authors }) => {
+  const [name, setName] = useState(null);
+  const [born, setBorn] = useState(null);
+
   const [editAuthor] = useMutation(EDIT_AUTHOR);
 
   if (!show) {
     return null;
   }
+
+  const submit = async (event) => {
+    event.preventDefault();
+  };
 
   return (
     <div>
@@ -27,6 +35,20 @@ const Authors = ({ show, authors }) => {
           ))}
         </tbody>
       </table>
+      <div>
+        <h2>Set birthyear</h2>
+        <form onSubmit={submit}>
+          <div>
+            name
+            <input placeholder="enter name"></input>
+          </div>
+          <div>
+            born
+            <input placeholder="enter year"></input>
+          </div>
+          <button type="submit">update author</button>
+        </form>
+      </div>
     </div>
   );
 };
