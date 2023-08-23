69a21437417843101bb85c3ce56487d2e7b4d972
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-23 16:50:01 +0200

    add query for fetching all books data

--- library-frontend/src/queries.js
+++ library-frontend/src/queries.js
@@ -11,6 +11,16 @@ export const ALL_AUTHORS = gql`
   }
 `;
 
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+    }
+  }
+`;
+
 export const ADD_BOOK = gql`
   mutation addBook(
     $title: String!
