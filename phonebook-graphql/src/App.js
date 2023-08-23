44f25cbd70117fb1ab16c11dbb6f615f48ed3c61
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-23 09:34:15 +0200

    add error handling with notify component

--- phonebook-graphql/src/App.js
+++ phonebook-graphql/src/App.js
@@ -2,8 +2,12 @@ import { useQuery } from "@apollo/client";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import { ALL_PERSONS } from "./queries";
+import { useState } from "react";
+import Notify from "./components/Notify";
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const result = useQuery(ALL_PERSONS, {
     pollInterval: 2000,
   });
@@ -12,10 +16,18 @@ const App = () => {
     return <div>loading...</div>;
   }
 
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
     </div>
   );
 };
