d6ba6cc34736b6215a24c8a5c8cd7dd2e6601b37
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-24 18:03:53 +0200

    clean code

--- library-backend/library-backend.js
+++ library-backend/library-backend.js
@@ -98,6 +98,9 @@ const resolvers = {
         throw new Error("Failed to fetch authors");
       }
     },
+    me: (root, args, context) => {
+      return context.currentUser;
+    },
   },
   Author: {
     bookCount: (root) => books.filter((b) => b.author === root.name).length,
@@ -189,10 +192,22 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true,
 });
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.startsWith("Bearer ")) {
+      const decodedToken = jwt.verify(
+        auth.substring(7),
+        process.env.JWT_SECRET
+      );
+      const currentUser = await User.findById(decodedToken.id);
+      return { currentUser };
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
