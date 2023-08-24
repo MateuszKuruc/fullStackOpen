fc16338364b50d9f13c2e79a097057505753f864
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-24 18:43:47 +0200

    add token state control for authentication, add query for logging in

--- phonebook-graphql/src/App.js
+++ phonebook-graphql/src/App.js
@@ -5,13 +5,13 @@ import { ALL_PERSONS } from "./queries";
 import { useState } from "react";
 import Notify from "./components/Notify";
 import PhoneForm from "./components/PhoneForm";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null);
+  const [token, setToken] = useState(null);
 
-  const result = useQuery(ALL_PERSONS, {
-    pollInterval: 2000,
-  });
+  const result = useQuery(ALL_PERSONS);
 
   if (result.loading) {
     return <div>loading...</div>;
@@ -23,6 +23,16 @@ const App = () => {
       setErrorMessage(null);
     }, 10000);
   };
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    );
+  }
 
   return (
     <div>
