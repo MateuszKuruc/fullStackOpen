55a57c6b4254276c7e92af7d96c693be7da51f45
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-26 00:37:26 +0200

    implement token status based rendering of the page

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -6,6 +6,28 @@ import LoginForm from "./components/LoginForm";
 
 const App = () => {
   const [page, setPage] = useState("authors");
+  const [token, setToken] = useState(null);
+
+  if (!token) {
+    return (
+      <div>
+        <div>
+          <button onClick={() => setPage("authors")}>authors</button>
+          <button onClick={() => setPage("books")}>books</button>
+
+          <button onClick={() => setPage("login")}>login</button>
+        </div>
+
+        <Authors show={page === "authors"} />
+
+        <Books show={page === "books"} />
+
+        <NewBook show={page === "add"} />
+
+        <LoginForm show={page === "login"} />
+      </div>
+    );
+  }
 
   return (
     <div>
