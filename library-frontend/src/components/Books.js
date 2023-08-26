48d2fb406ffc92ef8b3e69575277cb33ea78e4be
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-26 18:45:51 +0200

    ex 8.19 done, introduce filtering in book component to display books based on the genre chosen by user

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,10 +1,10 @@
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const Books = ({ show }) => {
   const { loading, data } = useQuery(ALL_BOOKS);
-  const [filteredBooks, setFilteredBooks] = useState("all");
+  const [filteredBooks, setFilteredBooks] = useState(null);
 
   if (!show) {
     return null;
@@ -16,12 +16,14 @@ const Books = ({ show }) => {
 
   const books = data.allBooks;
   console.log("books:", books);
+  console.log("filtered books:", filteredBooks);
 
   const filterBooksByGenre = (genre) => {
     if (!genre) {
       return setFilteredBooks(books);
     } else {
       const filtered = books.filter((book) => book.genres.includes(genre));
+      console.log("filtered", filtered);
       setFilteredBooks(filtered);
     }
   };
@@ -37,7 +39,7 @@ const Books = ({ show }) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {(!filteredBooks ? books : filteredBooks).map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -49,11 +51,15 @@ const Books = ({ show }) => {
       </table>
       <div>
         <h2>Filters</h2>
-        <button onClick={() => filterBooksByGenre("all")}>All books</button>
-        <button onClick={() => filterBooksByGenre("jajco")}>Fantasy</button>
-        <button>Science-fiction</button>
-        <button>History</button>
-        <button>Technology</button>
+        <button onClick={() => filterBooksByGenre()}>All books</button>
+        <button onClick={() => filterBooksByGenre("Fantasy")}>Fantasy</button>
+        <button onClick={() => filterBooksByGenre("Science-fiction")}>
+          Science-fiction
+        </button>
+        <button onClick={() => filterBooksByGenre("History")}>History</button>
+        <button onClick={() => filterBooksByGenre("Technology")}>
+          Technology
+        </button>
       </div>
     </div>
   );
