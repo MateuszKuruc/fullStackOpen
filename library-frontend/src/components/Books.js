958f1d197121718407e731bbc0e649017499799a
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-26 18:35:15 +0200

    edit graphql query to fetch genres data for books and display it. Add functions and buttons for filtering genre

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,8 +1,10 @@
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
+import { useEffect, useState } from "react";
 
 const Books = ({ show }) => {
   const { loading, data } = useQuery(ALL_BOOKS);
+  const [filteredBooks, setFilteredBooks] = useState("all");
 
   if (!show) {
     return null;
@@ -13,6 +15,16 @@ const Books = ({ show }) => {
   }
 
   const books = data.allBooks;
+  console.log("books:", books);
+
+  const filterBooksByGenre = (genre) => {
+    if (!genre) {
+      return setFilteredBooks(books);
+    } else {
+      const filtered = books.filter((book) => book.genres.includes(genre));
+      setFilteredBooks(filtered);
+    }
+  };
 
   return (
     <div>
@@ -30,10 +42,19 @@ const Books = ({ show }) => {
               <td>{a.title}</td>
               <td>{a.author.name}</td>
               <td>{a.published}</td>
+              <td>{a.genres.join(", ")}</td>
             </tr>
           ))}
         </tbody>
       </table>
+      <div>
+        <h2>Filters</h2>
+        <button onClick={() => filterBooksByGenre("all")}>All books</button>
+        <button onClick={() => filterBooksByGenre("jajco")}>Fantasy</button>
+        <button>Science-fiction</button>
+        <button>History</button>
+        <button>Technology</button>
+      </div>
     </div>
   );
 };
