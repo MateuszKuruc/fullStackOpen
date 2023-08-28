6219e98ce68144ff16a5d95e252e91810d4f9ac1
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-28 14:53:31 +0200

    add function calculations for all the needed values for result object that is to be returned

--- typescript-bmi/exerciseCalculator.ts
+++ typescript-bmi/exerciseCalculator.ts
@@ -8,17 +8,45 @@ interface Result {
   average: number;
 }
 
-const calculateExercises = (data: [], dailyTarget: number) => {
+const calculateExercises = (data: number[], dailyTarget: number): Result => {
   const periodLength = data.length;
   const trainingDays = data.filter((d) => d !== 0).length;
-  const targetValue = dailyTarget;
+  const target = dailyTarget;
   const allHours = data.reduce((sum, number) => {
     return sum + number;
   }, 0);
-  const averageHours = allHours / periodLength;
+  const average = allHours / periodLength;
+  const success = average > target ? true : false;
+  let rating;
+  if (periodLength === trainingDays) {
+    rating = 3;
+  } else if ((trainingDays / periodLength) * 100 > 80) {
+    rating = 2;
+  } else {
+    rating = 1;
+  }
+
+  let ratingDescription;
+  if (rating === 3) {
+    ratingDescription = "very well done!";
+  } else if (rating === 2) {
+    ratingDescription = "not too bad!";
+  } else {
+    ratingDescription = "need to try harder!";
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
 };
 
-const data = [1, 2, 3, 4, 5];
+// const data = [1, 2, 3, 4, 5];
 // const allHours = data.reduce((sum, number) => {
 //   return sum + number;
 // }, 0);
