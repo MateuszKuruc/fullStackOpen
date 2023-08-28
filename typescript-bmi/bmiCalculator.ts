6c3a89576ca01720a38c3925da18dd234d471c0d
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-28 13:16:29 +0200

    fix ex 9.1, change the return type and value based on the number calculated from parameters

--- typescript-bmi/bmiCalculator.ts
+++ typescript-bmi/bmiCalculator.ts
@@ -1,5 +1,12 @@
-const calculateBmi = (height: number, weight: number): number => {
-  return weight / ((height / 100) * (height / 100));
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / ((height / 100) * (height / 100));
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi > 18.4 && bmi < 25) {
+    return "Normal weight";
+  } else if (bmi > 24.9 && bmi < 30) {
+    return "Overweight";
+  } else if (bmi > 30) return "Obese";
 };
 
 console.log(calculateBmi(190, 90));
