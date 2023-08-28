d8efa13feb4945a4e449164cd98f938de2952f23
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-28 14:31:44 +0200

    ex 9.2, add interface for Result object and start working on the function body

--- typescript-bmi/exerciseCalculator.ts
+++ typescript-bmi/exerciseCalculator.ts
@@ -1,1 +1,29 @@
-const calculateExercises = 
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (data: [], dailyTarget: number) => {
+  const periodLength = data.length;
+  const trainingDays = data.filter((d) => d !== 0).length;
+  const targetValue = dailyTarget;
+  const allHours = data.reduce((sum, number) => {
+    return sum + number;
+  }, 0);
+  const averageHours = allHours / periodLength;
+};
+
+const data = [1, 2, 3, 4, 5];
+// const allHours = data.reduce((sum, number) => {
+//   return sum + number;
+// }, 0);
+
+// console.log(allHours);
+
+// const trainingDays = data.filter((d) => d !== 0).length;
+// console.log(trainingDays);
