72ab1e6f799775a263e916edf0c11ef43f146784
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-28 18:47:58 +0200

    ex 9.3, bmi calculator rewritten to accept command line arguments

--- typescript-bmi/bmiCalculator.ts
+++ typescript-bmi/bmiCalculator.ts
@@ -1,12 +1,19 @@
-const calculateBmi = (height: number, weight: number): string => {
+type Assessment = "Underweight" | "Normal weight" | "Overweight" | "Obese";
+
+const calculateBmi = (height: number, weight: number): Assessment => {
   const bmi = weight / ((height / 100) * (height / 100));
   if (bmi < 18.5) {
     return "Underweight";
-  } else if (bmi > 18.4 && bmi < 25) {
+  } else if (bmi < 25) {
     return "Normal weight";
-  } else if (bmi > 24.9 && bmi < 30) {
+  } else if (bmi < 30) {
     return "Overweight";
   } else if (bmi > 30) return "Obese";
 };
 
-console.log(calculateBmi(190, 90));
+const height: number = Number(process.argv[2]);
+const weight: number = Number(process.argv[3]);
+
+console.log(calculateBmi(height, weight));
+
+// console.log(calculateBmi(190, 90));
