f69856eeda8d1e2ceb328b9615ddbd1d890171a3
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-29 22:05:45 +0200

    fix issue with cors for exercises 9.9 and 9.11

--- patientor-app/patientor-backend/index.ts
+++ patientor-app/patientor-backend/index.ts
@@ -3,7 +3,10 @@ import diagnosisRouter from "./src/routes/diagnoses_router";
 import patientsRouter from "./src/routes/patients_router";
 
 const app = express();
+const cors = require("cors");
+
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3001;
 
