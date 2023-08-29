3ffa838c6f19594fc7f8b28ad774c2efa42a51ab
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-29 13:31:58 +0200

    update port

--- patientor-app/patientor-backend/index.ts
+++ patientor-app/patientor-backend/index.ts
@@ -3,10 +3,10 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-const PORT = 3003;
+const PORT = 3001;
 
 app.get("/api/ping", (_req, res) => {
-  console.log("test pinging");
+  console.log("someone pinged");
   res.send("pooooong");
 });
 
