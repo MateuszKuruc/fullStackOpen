458bba315fbd319ce5a36e4fc69a488d946c4d7a
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-30 18:53:35 +0200

    introduce deeper type usage, define interfaces for different course groups

--- ts-react/types.tsx
+++ ts-react/types.tsx
@@ -6,3 +6,30 @@ export interface ContentProps {
   name: string;
   exerciseCount: number;
 }
+
+interface CoursePartBasic {
+  name: string;
+  exerciseCount: number;
+  description: string;
+  kind: "basic";
+}
+
+interface CoursePartGroup {
+  name: string;
+  exerciseCount: number;
+  groupProjectCount: number;
+  kind: "group";
+}
+
+interface CoursePartBackground {
+  name: string;
+  exerciseCount: number;
+  description: string;
+  backgroundMaterial: string;
+  kind: "background";
+}
+
+export type CoursePart =
+  | CoursePartBasic
+  | CoursePartGroup
+  | CoursePartBackground;
