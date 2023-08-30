ddf42304c6feb450bf01324a8574e6faeeb8df04
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-30 23:13:48 +0200

    ex 9.15, write part component with switch case-based exhaustive type checking for passed data to render all attributes

--- ts-react/src/components/Part.tsx
+++ ts-react/src/components/Part.tsx
@@ -5,15 +5,35 @@ interface PartProps {
 }
 
 const Part = ({ part }: PartProps) => {
- switch (part.kind) {
+  switch (part.kind) {
     case "basic":
-        return (
-            <div>
-                <h3>{part.name}</h3>
-                <p>{part.exerciseCount}</p>
-            </div>
-        )
- }
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercise count: {part.exerciseCount}</p>
+          <p>Description: {part.description}</p>
+        </div>
+      );
+      break;
+    case "group":
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercise count: {part.exerciseCount}</p>
+          <p>Group project count: {part.groupProjectCount}</p>
+        </div>
+      );
+      break;
+    case "background":
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercise count: {part.exerciseCount}</p>
+          <p>Description: {part.description}</p>
+          <p>Background material: {part.backgroundMaterial}</p>
+        </div>
+      );
+  }
 };
 
 export default Part;
