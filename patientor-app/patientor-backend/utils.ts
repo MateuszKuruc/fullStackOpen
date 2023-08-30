179e96298f7fce31f8f2cd2f1e4d13952ee7ed9c
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-30 13:55:54 +0200

    ex 9.13 done, add safe parsing, validation and type predicate to POST request for patients, use enum for gender field

--- patientor-app/patientor-backend/utils.ts
+++ patientor-app/patientor-backend/utils.ts
@@ -1,11 +1,17 @@
-import { NewPatientEntry } from "./types";
+import { NewPatientEntry, Gender } from "./types";
 
 const isString = (text: unknown): text is string => {
   return typeof text === "string" || text instanceof String;
 };
 
 const isDate = (date: string): boolean => {
-  return Boolean[Symbol](Date.parse(date));
+  return Boolean(Date.parse(date));
+};
+
+const isGender = (param: string): param is Gender => {
+  return Object.values(Gender)
+    .map((v) => v.toString())
+    .includes(param);
 };
 
 const parseName = (name: unknown): string => {
@@ -33,17 +39,43 @@ const parseSsn = (ssn: unknown): string => {
 };
 
 const parseOccupation = (occupation: unknown): string => {
-  if (!occupation || isString(occupation)) {
+  if (!occupation || !isString(occupation)) {
     throw new Error("Incorrect or missing occupation");
   }
 
   return occupation;
 };
 
-const toNewPatientEntry = (object: unknown): NewPatientEntry => {
-  const newEntry: NewPatientEntry = {};
+const parseGender = (gender: unknown): Gender => {
+  if (!gender || !isString(gender) || !isGender(gender)) {
+    throw new Error("Incorrect or missing gender:" + gender);
+  }
+  return gender;
+};
 
-  return newEntry;
+const toNewPatientEntry = (object: unknown): NewPatientEntry => {
+  if (!object || typeof object !== "object") {
+    throw new Error("Incorrect or missing data");
+  }
+
+  if (
+    "name" in object &&
+    "dateOfBirth" in object &&
+    "ssn" in object &&
+    "gender" in object &&
+    "occupation" in object
+  ) {
+    const newEntry: NewPatientEntry = {
+      name: parseName(object.name),
+      dateOfBirth: parseDate(object.dateOfBirth),
+      ssn: parseSsn(object.ssn),
+      gender: parseGender(object.gender),
+      occupation: parseOccupation(object.occupation),
+    };
+    return newEntry;
+  }
+
+  throw new Error("Incorrect data: some fields are missing");
 };
 
 export default toNewPatientEntry;
