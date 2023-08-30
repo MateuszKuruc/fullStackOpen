e7be1a80b33121ccb5d1552bee7583129c3dd1bc
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-30 12:18:55 +0200

    ex 9.13, add type guards in utils file to check the validity of data types passed to create new patient

--- patientor-app/patientor-backend/utils.js
+++
@@ -1,9 +0,0 @@
-import { NewPatientEntry } from "./types";
-
-const toNewPatientEntry = (object: unknown): NewPatientEntry => {
-  const newEntry: NewPatientEntry = {};
-
-  return newEntry;
-};
-
-export default toNewPatientEntry;
