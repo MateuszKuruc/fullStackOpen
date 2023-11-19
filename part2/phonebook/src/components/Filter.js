fe07064e20e85bc90403e3b87e2882cacfed776a
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-19 04:12:18 +0100

    change header styling

--- part2/phonebook/src/components/Filter.js
+++ part2/phonebook/src/components/Filter.js
@@ -1,7 +1,12 @@
+import { FaArrowDown } from "react-icons/fa";
+
 const Filter = (props) => {
   return (
     <div className="filterContainer">
-      <h3>Search for contact</h3>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <h3>Search for contact</h3>
+        <FaArrowDown size={30} />
+      </div>
       <input className="filter" onChange={props.handleInput} />
     </div>
   );
